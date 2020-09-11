f6a4827cb8ec3a4ae38100305db09668f3fa4740
Elizabeth Needham <EliNeedham@outlook.com>
2020-09-10 21:38:24 -0500

    added a photo to the leadership team page, tried to make a lightbox but couldn't figure it out

---
+++ FED1StudioProject/JS/JavaScript.js
@@ -0,0 +1,27 @@
+﻿
+
+$(function () {
+
+$(".thumb").on("click", function () {//grab all objects with a class of thumb collecting data from the img source when they click on the image
+
+    var imgSource = $(this).attr("src");
+    //console.log("imgSource: " + imgSource); check if it is capturing the stuff
+    //make reference to lightbox content
+
+    var imgString = "<img src='" + imgSource + "' class='img-responsive' />"
+    console.log("imgString: " + imgString);
+
+    $("#lightbox-content").html(imgString);
+
+
+    //$("#lightbox-content").html("<img src='" + imgSource + "' class='img-responsive' />"); //trying to creat an html object but you're doing it in string
+
+    // make lightbox visible by fading it ia
+    $("#lightbox-container").fadeIn(500);
+
+    // when the user clicks anywhere in the #lightbox-container then fadeout()
+    $("#lightbox-container").on("click", function () {
+        $(this).fadeOut(500);
+    })
+
+});
